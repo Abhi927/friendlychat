98f55628336456406347b7c5611bf7b16fda4238
abhinav <resoabhisingh@gmail.com>
2024-05-12 20:42:20 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component ,inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatMenuModule} from '@angular/material/menu';
+import { FirebaseService } from './service/firebase.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatMenuModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers:[FirebaseService]
 })
 export class AppComponent {
   title = 'friendlychat';
+  constructor(private fireService:FirebaseService){}
+  
+  ngOnInit():void{
+}
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({apiKey: "AIzaSyB-Nz9a-EqbbCOM9qaZOp5eehHEzgz6Iuc",
+  authDomain: "friendlychat-a895c.firebaseapp.com",
+  projectId: "friendlychat-a895c",
+  storageBucket: "friendlychat-a895c.appspot.com",
+  messagingSenderId: "793532508405",
+  appId: "1:793532508405:web:b1eecfb9fb90b2de6e7dbc",
+  measurementId: "G-QSY5XC30PX"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideFunctions(() => getFunctions())), importProvidersFrom(provideMessaging(() => getMessaging())), importProvidersFrom(provideStorage(() => getStorage()))]
 };
